15cbfaa7b5fdb69be009783100d7c4138a06777b
Adler <1101635162@qq.com>
2019-11-30 23:06:13 +0800

    :bug: 跟随更新3.6.7：🎨 Cleanup code

--- src/main/webapp/js/admin/linkList.js
+++ src/main/webapp/js/admin/linkList.js
@@ -20,7 +20,7 @@
  *
  * @author <a href="http://vanessa.b3log.org">Liyuan Li</a>
  * @author <a href="http://88250.b3log.org">Liang Ding</a>
- * @version 1.1.2.2, Oct 23, 2019
+ * @version 1.1.2.3, Oct 24, 2019
  */
 
 /* link-list 相关操作 */

ed0bbee4f586ed43d1e8b04970137f3553df4bd6
adlered <adlered@outlook.com>
2020-08-13 22:46:47 +0800

    :art: 支持将文章同步至GitHub仓库 #104

--- src/main/java/org/b3log/solo/util/GitHubs.java
+++ src/main/java/org/b3log/solo/util/GitHubs.java
@@ -30,6 +30,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
 
 /**
  * GitHub utilities.
@@ -64,9 +65,7 @@ public final class GitHubs {
                 return null;
             }
             final JSONObject data = result.optJSONObject(Common.DATA);
-            final JSONArray ret = data.optJSONArray("githubrepos");
-
-            return ret;
+            return data.optJSONArray("githubrepos");
         } catch (final Exception e) {
             LOGGER.log(Level.ERROR, "Gets GitHub repos failed", e);
 
@@ -75,43 +74,122 @@ public final class GitHubs {
     }
 
     /**
-     * Gets GitHub user info.
+     * Updates a file by the specified personal access token, GitHub login name, repo name, file path and file content.
      *
-     * @param accessToken the specified access token
-     * @return GitHub user info, for example, <pre>
-     * {
-     *   "openId": "",
-     *   "userName": "D",
-     *   "userAvatar": "https://avatars3.githubusercontent.com/u/873584?v=4"
-     * }
-     * </pre>, returns {@code null} if not found QQ user info
+     * @param pat       the specified personal access token
+     * @param loginName the specified GitHub login name
+     * @param repoName  the specified repo name
+     * @param filePath  the specfiied file name
+     * @param content   the specified file content
+     * @return {@code true} if ok, returns {@code false} if failed
      */
-    public static JSONObject getGitHubUserInfo(final String accessToken) {
+    public static boolean updateFile(final String pat, final String loginName, final String repoName, final String filePath, final byte[] content) {
+        final String fullRepoName = loginName + "/" + repoName;
         try {
-            final HttpResponse res = HttpRequest.get("https://hacpai.com/github/user?ak=" + accessToken).trustAllCerts(true).
-                    connectionTimeout(3000).timeout(7000).header("User-Agent", Solos.USER_AGENT).send();
-            if (HttpServletResponse.SC_OK != res.statusCode()) {
-                return null;
+            HttpResponse response = HttpRequest.get("https://api.github.com/repos/" + fullRepoName + "/git/trees/master").header("Authorization", "token " + pat).
+                    connectionTimeout(7000).timeout(60000).header("User-Agent", Solos.USER_AGENT).send();
+            int statusCode = response.statusCode();
+            response.charset("UTF-8");
+            String responseBody = response.bodyText();
+            if (200 != statusCode && 409 != statusCode) {
+                LOGGER.log(Level.ERROR, "Get git tree of file [" + filePath + "] failed: " + responseBody);
+                return false;
             }
-            res.charset("UTF-8");
-            final JSONObject result = new JSONObject(res.bodyText());
-            if (0 != result.optInt(Keys.STATUS_CODE)) {
-                return null;
+
+            final JSONObject body = new JSONObject().
+                    put("message", ":memo: 更新博客").
+                    put("content", Base64.getEncoder().encodeToString(content));
+            if (200 == statusCode) {
+                final JSONObject responseData = new JSONObject(responseBody);
+                final JSONArray tree = responseData.optJSONArray("tree");
+                for (int i = 0; i < tree.length(); i++) {
+                    final JSONObject file = tree.optJSONObject(i);
+                    if (StringUtils.equals(filePath, file.optString("path"))) {
+                        body.put("sha", file.optString("sha"));
+                        break;
+                    }
+                }
             }
-            final JSONObject data = result.optJSONObject(Common.DATA);
-            final String userName = StringUtils.trim(data.optString("userName"));
-            final String openId = data.optString("userId");
-            final String avatarUrl = data.optString("userAvatarURL");
 
-            final JSONObject ret = new JSONObject();
-            ret.put("openId", openId);
-            ret.put(User.USER_NAME, userName);
-            ret.put(UserExt.USER_AVATAR, avatarUrl);
-
-            return ret;
+            response = HttpRequest.put("https://api.github.com/repos/" + fullRepoName + "/contents/" + filePath).header("Authorization", "token " + pat).
+                    connectionTimeout(7000).timeout(60000 * 2).header("User-Agent", Solos.USER_AGENT).bodyText(body.toString()).send();
+            statusCode = response.statusCode();
+            response.charset("UTF-8");
+            responseBody = response.bodyText();
+            if (200 != statusCode && 201 != statusCode) {
+                LOGGER.log(Level.ERROR, "Updates repo [" + repoName + "] file [" + filePath + "] failed: " + responseBody);
+                return false;
+            }
+            return true;
         } catch (final Exception e) {
-            LOGGER.log(Level.ERROR, "Gets GitHub user info failed", e);
+            LOGGER.log(Level.ERROR, "Updates repo [" + repoName + "] file [" + filePath + "] failed: " + e.getMessage());
+            return false;
+        }
+    }
 
+    /**
+     * Creates or updates a GitHub repository by the specified personal access token, GitHub login name, repo name repo desc and repo homepage.
+     *
+     * @param pat          the specified personal access token
+     * @param loginName    the specified GitHub login name
+     * @param repoName     the specified repo name
+     * @param repoDesc     the specified repo desc
+     * @param repoHomepage the specified repo homepage
+     * @return {@code true} if ok, returns {@code false} if failed
+     */
+    public static boolean createOrUpdateGitHubRepo(final String pat, final String loginName, final String repoName, final String repoDesc, final String repoHomepage) {
+        try {
+            final JSONObject body = new JSONObject().
+                    put("name", repoName).
+                    put("description", repoDesc).
+                    put("homepage", repoHomepage).
+                    put("has_wiki", false).
+                    put("has_projects", false);
+            HttpResponse response = HttpRequest.post("https://api.github.com/user/repos").header("Authorization", "token " + pat).
+                    connectionTimeout(7000).timeout(30000).header("User-Agent", Solos.USER_AGENT).bodyText(body.toString()).send();
+            int statusCode = response.statusCode();
+            response.charset("UTF-8");
+            String responseBody = response.bodyText();
+            if (201 != statusCode && 422 != statusCode) {
+                LOGGER.log(Level.ERROR, "Creates GitHub repo [" + repoName + "] failed: " + responseBody);
+                return false;
+            }
+            if (201 == statusCode) {
+                return true;
+            }
+
+            response = HttpRequest.patch("https://api.github.com/repos/" + loginName + "/" + repoName).header("Authorization", "token " + pat).
+                    connectionTimeout(7000).timeout(30000).header("User-Agent", Solos.USER_AGENT).bodyText(body.toString()).send();
+            statusCode = response.statusCode();
+            responseBody = response.bodyText();
+            if (200 != statusCode) {
+                LOGGER.log(Level.ERROR, "Updates GitHub repo [" + repoName + "] failed: " + responseBody);
+                return false;
+            }
+            return true;
+        } catch (final Exception e) {
+            LOGGER.log(Level.ERROR, "Creates or updates GitHub repo failed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Gets GitHub user by the specified personal access token.
+     *
+     * @param pat the specified personal access token
+     * @return GitHub user, returns {@code null} if failed
+     */
+    public static JSONObject getGitHubUser(final String pat) {
+        try {
+            final HttpResponse response = HttpRequest.get("https://api.github.com/user").header("Authorization", "token " + pat).
+                    connectionTimeout(7000).timeout(30000).header("User-Agent", Solos.USER_AGENT).send();
+            if (200 != response.statusCode()) {
+                return null;
+            }
+            response.charset("UTF-8");
+            return new JSONObject(response.bodyText());
+        } catch (final Exception e) {
+            LOGGER.log(Level.ERROR, "Gets GitHub user info failed: " + e.getMessage());
             return null;
         }
     }

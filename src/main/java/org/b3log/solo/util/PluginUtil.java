5abcfa94bb0499297e5c1d57829cd54f7986c926
adler <1101635162@qq.com>
2021-04-05 00:10:07 +0800

    :art: b3log社区功能整合为插件功能 #160

--- src/main/java/org/b3log/solo/util/PluginUtil.java
+++ src/main/java/org/b3log/solo/util/PluginUtil.java
@@ -1,18 +1,17 @@
 package org.b3log.solo.util;
 
-import org.b3log.latke.ioc.Inject;
+import org.b3log.latke.ioc.BeanManager;
 import org.b3log.solo.repository.PluginRepository;
-import org.b3log.solo.service.PluginQueryService;
 
 /**
  * Get Bolo Plugins status.
  */
 public class PluginUtil {
 
-    @Inject
-    PluginRepository pluginRepository;
+    final static BeanManager beanManager = BeanManager.getInstance();
+    final static PluginRepository pluginRepository = beanManager.getReference(PluginRepository.class);
 
-    public boolean b3logPluginEnabled() {
+    public static boolean b3logPluginEnabled() {
         try {
             return pluginRepository.get("B3log支持插件_0.0.1").optString("status").equals("ENABLED");
         } catch (Exception e) {

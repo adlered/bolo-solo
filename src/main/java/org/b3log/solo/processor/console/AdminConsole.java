f138c72b771d38d0f3066105e066de4f4c39516e
adler <1101635162@qq.com>
2021-04-21 22:26:54 +0800

    :art: b3log社区功能整合为插件功能 #160

--- src/main/java/org/b3log/solo/processor/console/AdminConsole.java
+++ src/main/java/org/b3log/solo/processor/console/AdminConsole.java
@@ -163,6 +163,7 @@ public class AdminConsole {
             dataModel.put(Common.VERSION, SoloServletListener.VERSION);
             dataModel.put(Common.BOLO_VERSION, SoloServletListener.BOLO_VERSION);
             dataModel.put(Common.STATIC_RESOURCE_VERSION, Latkes.getStaticResourceVersion());
+            dataModel.put(Common.B3LOG_ENABLED, "" + PluginUtil.b3logPluginEnabled());
             dataModel.put(Common.YEAR, String.valueOf(Calendar.getInstance().get(Calendar.YEAR)));
             dataModel.put(Option.ID_C_ARTICLE_LIST_DISPLAY_COUNT, preference.getInt(Option.ID_C_ARTICLE_LIST_DISPLAY_COUNT));
             dataModel.put(Option.ID_C_ARTICLE_LIST_PAGINATION_WINDOW_SIZE, preference.getInt(Option.ID_C_ARTICLE_LIST_PAGINATION_WINDOW_SIZE));
